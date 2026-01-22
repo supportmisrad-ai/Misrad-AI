20ca9c6999e2605ee125312474dbaefe0966d271
itsikdahan1 <itsikdahan1@gmail.com>
2026-01-22 23:15:27 +0200

    Delete: System Legacy routes and SystemApp.

--- app/w/[orgSlug]/(modules)/system/SystemModuleEntryClient.tsx
+++
@@ -1,9 +0,0 @@
-import SystemModuleClient from './SystemModuleClient';
-
-export default function SystemModuleEntryClient(props: {
-  initialCurrentUser?: any;
-  initialOrganization?: any;
-  initialTab?: string;
-}) {
-  return <SystemModuleClient {...props} />;
-}
