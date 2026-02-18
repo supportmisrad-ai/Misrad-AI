a8cd21215f00b444553f3374cabd6dd2fa0dee8f
itsikdahan1 <itsikdahan1@gmail.com>
2026-02-18 17:56:41 +0200

    chore: remove accidental remotion files (moved to feature branch)

--- remotion.config.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '@remotion/cli/config';
-
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
-Config.setPixelFormat('yuv420p');
-Config.setCodec('h264');
-Config.setCrf(18);
